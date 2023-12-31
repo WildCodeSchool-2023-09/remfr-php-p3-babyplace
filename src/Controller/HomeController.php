391f8e07b47081ade45df4c550ee7ec5ed64be53
Julien PRIVAT <contact@julien-privat.com>
2023-12-31 14:43:20 +0100

    Démarrage de la page accueil et du layout website

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -31,4 +31,17 @@ class HomeController extends AbstractController
     {
         return $this->render('choice.html.twig');
     }
+
+    #[Route('/administration', name: 'administration')]
+    public function dashboard(): Response
+    {
+        return $this->render('/dashboard-creche.html.twig');
+    }
+
+    /* Website */
+    #[Route('/accueil', methods: ['GET', 'POST'], name: 'accueil')]
+    public function home(): Response
+    {
+        return $this->render('website/home.html.twig');
+    }
 }
