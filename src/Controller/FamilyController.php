e3c601e5d3f3f198ed53879f62344f0ae498e4c7
Julien PRIVAT <contact@julien-privat.com>
2024-01-02 20:26:24 +0100

    FamilyController

---
+++ src/Controller/FamilyController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FamilyController extends AbstractController
+{
+    #[Route('/parent', name: 'parent')]
+    public function index(): Response
+    {
+        return $this->render('parent/index.html.twig', [
+            'controller_name' => 'FamilyController',
+        ]);
+    }
+
+    #[Route('/menu-parent', name: 'menu')]
+    public function menuParent(): Response
+    {
+        return $this->render('parent/menu.html.twig', [
+            'controller_name' => 'FamilyController',
+        ]);
+    }
+}
