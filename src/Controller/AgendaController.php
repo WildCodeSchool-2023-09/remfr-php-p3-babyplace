5fcf020eb6712f5059e7949d861496d804cc3401
William-lh <lherm.william@gmail.com>
2024-01-03 15:41:14 +0100

    en cours agenda

---
+++ src/Controller/AgendaController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AgendaController extends AbstractController
+{
+    #[Route('/agenda', name: 'agenda_')]
+    public function index(): Response
+    {
+        return $this->render('agenda/agenda.html.twig');
+    }
+}
