38f23671649f23bf27e74121dbaa2705686d8343
elisechldn <elise.chaldjian@gmail.com>
2024-01-04 15:16:55 +0100

    Ajout du traitement du formulaire dans le controller

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\BrowserKit\Response as BrowserKitResponse;
+use Symfony\Flex\Response as FlexResponse;
 
 #[Route('/administration', name: 'administration_')]
 class AdministrationController extends AbstractController
@@ -18,5 +19,25 @@ class AdministrationController extends AbstractController
     public function index(): Response
     {
         return $this->render('administration/index.html.twig');
+    }
+
+    #[Route('/add', name:'add')]
+    public function add(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        //Création d'un nouvel objet administratif
+        $administration = new Administration();
+        //Ajouter un lien avec le formulaire associé
+        $form = $this->createForm(AdministrationType::class, $administration);
+        //Récupération de la data issue du HTTP Request
+        $form->handleRequest($request);
+        //A la soumission du formulaire
+        if ($form->isSubmitted()) {
+            $entityManager->persist($administration);
+            $entityManager->flush();
+        }
+
+        return $this->render('administration/index.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
