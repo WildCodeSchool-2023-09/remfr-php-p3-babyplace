758216450db7c48c5cddd1394700663612bab8c8
William-lh <lherm.william@gmail.com>
2024-01-05 11:39:25 +0100

    fix de la dev

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,13 +11,7 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
-    }
-
-    #[Route('/inscription', methods: ['GET', 'POST'], name: 'inscription')]
-    public function register(): Response
-    {
-        return $this->render('register.html.twig');
+        return $this->render('website/home.html.twig');
     }
 
     #[Route('/choix', methods: ['GET', 'POST'], name: 'choix')]
@@ -30,12 +24,5 @@ class HomeController extends AbstractController
     public function dashboard(): Response
     {
         return $this->render('/dashboard-creche.html.twig');
-    }
-
-    /* Website */
-    #[Route('/accueil', methods: ['GET', 'POST'], name: 'accueil')]
-    public function home(): Response
-    {
-        return $this->render('website/home.html.twig');
     }
 }
