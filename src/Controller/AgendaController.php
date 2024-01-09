5ce6cf4bbd9cf1d2f40a8d1f20840a5fa20d891a
William-lh <lherm.william@gmail.com>
2024-01-09 10:04:24 +0100

    update agenda

--- src/Controller/AgendaController.php
+++ src/Controller/AgendaController.php
@@ -2,15 +2,35 @@
 
 namespace App\Controller;
 
+use App\Repository\CalendarRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AgendaController extends AbstractController
 {
-    #[Route('/agenda', name: 'agenda_')]
-    public function index(): Response
+    #[Route('/agenda', name: 'app_agenda')]
+    public function index(CalendarRepository $calendarRepository): Response
     {
-        return $this->render('agenda/agenda.html.twig');
+        $events = $calendarRepository->findAll();
+
+        $rdvs = [];
+
+        foreach ($events as $event) {
+            $rdvs[] = [
+                'id' => $event->getId(),
+                'start' => $event->getStart()->format('Y-m-d H:i:s'),
+                'end' => $event->getEnd()->format('Y-m-d H:i:s'),
+                'title' => $event->getTitle(),
+                'description' => $event->getDescription(),
+                'backgroundColor' => $event->getBackgroundColor(),
+                'borderColor' => $event->getBorderColor(),
+                'textColor' => $event->getTextColor(),
+                'allDay' => $event->getAllDay(),
+            ];
+        }
+        $data = json_encode($rdvs);
+
+        return $this->render('agenda/agenda.html.twig', compact('data'));
     }
 }
