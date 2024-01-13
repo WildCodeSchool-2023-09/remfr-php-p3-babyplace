cf40f397d13e6163725618a9df0513b182fe041f
Julien PRIVAT <contact@julien-privat.com>
2024-01-13 23:06:53 +0100

    Fix du problème des icons de la navbar sur Recherche et Filtres

--- assets/app.js
+++ assets/app.js
@@ -15,7 +15,9 @@ window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 
 // Font Awesome
 require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
+// Agenda
 import './JS/calendar';
 import a2lix_lib from '@a2lix/symfony-collection/dist/a2lix_sf_collection.min'
 // import a2lix_lib from 'https://esm.run/@a2lix/symfony-collection/dist/a2lix_sf_collection.min';
