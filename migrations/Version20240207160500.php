76388a154531bd5ec06de7406fa3b0d054bce956
elisechldn <elise.chaldjian@gmail.com>
2024-02-07 17:17:14 +0100

    Correction du responsive du register

--- migrations/Version20240207160500.php
+++ migrations/Version20240207160500.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240206095714 extends AbstractMigration
+final class Version20240207160500 extends AbstractMigration
 {
     public function getDescription(): string
     {
